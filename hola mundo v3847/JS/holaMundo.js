2af6df45f1cfe9df61c43fff440a116ab8fbea1f
mym2013 <torresdelfierro@gmail.com>
2021-05-26 22:15:49 -0400

    primer commit

---
+++ hola mundo v3847/JS/holaMundo.js
@@ -0,0 +1,19 @@
+/*
+alert("bien venidos al curso de JS");
+alert("hola amigos comenzar"); 
+
+
+document.write("<h1>Quita el contenido viejo - Agrega el contenido nuevo!</h1>");
+document.write("partiendo por enensima vez");
+
+
+console.log("esta aparecer en la consola, asi que HOLA DESDE LA CONSILA");
+console.log(5+6);
+console.log("hola deade la consola");
+
+
+*/
+
+alert("bien venidos al curso de JS");
+alert("hola amigos comenzar"); 
+    
